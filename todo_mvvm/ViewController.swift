30970af72dcce34779ada18ae39e105e299144ec
Daniel Hakimi <daniel@appical.nl>
2019-08-26 08:51:56 +0200

    Initial Commit

---
+++ todo_mvvm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todo_mvvm
+//
+//  Created by Daniel Hakimi on 26/08/2019.
+//  Copyright © 2019 Daniel Hakimi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
